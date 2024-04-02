a0daebe427b49d655e9a52593bec445bee2d6e47
arthdarji352 <arthdarji20@gmail.com>
2024-04-02 22:11:40 +0530

    FE

---
+++ client/src/pages/LoginPAge.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "../styles/Login.scss";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      /* Get data after fetching */
+      const loggedIn = await response.json();
+
+      navigate("/");
+      toast.success("Login Successfuly");
+    } catch (err) {
+      console.log("Login failed", err.message);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In Here</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
